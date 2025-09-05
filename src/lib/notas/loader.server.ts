5d44d3f3384bcf2ea573c423fc8322bd35215577
Rogelio Echavarria <rogelioev0812@gmail.com>
2025-09-05 11:45:34 -0600

    Moved folder for notes

--- src/lib/notas/loader.server.ts
+++ src/lib/notas/loader.server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import { marked } from 'marked';
 
-const vaultPath = path.resolve('notas');
+const vaultPath = path.resolve('static/notas');
 
 function slugify(text: string): string {
   return text
